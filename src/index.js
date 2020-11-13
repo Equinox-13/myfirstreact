98d045f6658fb3b221edaed4669f128ac0523c23
Equinox-13 <quaid.johar33@gmail.com>
2020-11-13 20:02:59 +0530

    Added elements using JSX

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const myfirstelement = <h1>Hello React!</h1>
+const myfirstelement = <h1>Hello React!</h1>;
 
-ReactDOM.render(myfirstelement, document.getElementById('root'));
+const mysecondelement = (
+  <table>
+    <tr>
+      <th>Name</th>
+    </tr>
+    <tr>
+      <td>John</td>
+    </tr>
+    <tr>
+      <td>Elsa</td>
+    </tr>
+  </table>
+);
+
+const mythirdelement = <h1>React is {5 + 5} times better with JSX</h1>;
+
+const myfourthelement = (
+    <div>
+      <h1>I am a Header.</h1>
+      <h1>I am a Header too.</h1>
+    </div>
+);
+
+const myfifthelement = <input type="text" />;
+
+// ReactDOM.render(myfirstelement, document.getElementById('root'));
+// ReactDOM.render(mysecondelement, document.getElementById('root'));
+// ReactDOM.render(mythirdelement, document.getElementById('root'));
+// ReactDOM.render(myfourthelement, document.getElementById('root'));
+ReactDOM.render(myfifthelement, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
